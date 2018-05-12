00f0d17534402262372634b44e9f37a6088ec82e
chn555 <chn566@gmail.com>
2018-05-12 11:35:35 +0300

    started working

---
+++ functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# to get date -- date +%D
+# to get hour -- date +%H:%M
+
+Get_Time () {
+  Current_Time=$(date +%H:%M)
+}
+
+Get_Date () {
+  Current_Date=$(date +%D)
+}
+
+Get_ID () {
+  read -p "Please enter your 3 digit ID" Current_ID
+  until [[ Current_ID =~ [0-9]{1,3} ]]; do
+    echo "Invalid ID, try again "
+    read -p "Please enter your 3 digit ID" Current_ID
+  done
+}
+
+Get_Time
+Get_Date
+Get_ID
+
+printf "%s\n" $Current_Time $Current_Date $Current_ID
